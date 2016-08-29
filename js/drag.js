20658e5bc471e45ebb77cb1627596fc0be985b1e
Xernisfy <letterbox256@gmail.com>
2016-08-29 22:00:58 +0200

    Minor changes
    Really minor
    I wish it would be more...
    There's now a layer on top of the textarea to drag without interfering
    with the scrollbars, but typing is now difficult...
    And resizing is not implemented
    really minor...

--- js/drag.js
+++ js/drag.js
@@ -1,1 +1,46 @@
-var drag=function(){return{move:function(d,x,y){d.style.left=x+'px';d.style.top=y+'px';},startMoving:function(d,c,e){e=e||window.event;var c=document.getElementById(c),x=e.clientX,y=e.clientY,l=d.style.left,t=d.style.top,eh=d.clientHeight,ew=d.clientWidth,ch=c.clientHeight,cw=c.clientWidth;c.style.cursor='move';l=l.replace('px','');t=t.replace('px','');var dx=x-l,dy=y-t;document.onmousemove=function(e){e=e||window.event;var x=e.clientX,y=e.clientY,ax=x-dx,ay=y-dy;if(ax<0)ax=0;if(ay<0)ay=0;if(ax+ew>cw)ax=cw-ew;if(ay+eh>ch)ay=ch-eh;drag.move(d,ax,ay);}},stopMoving:function(c){var a=document.createElement('script');document.getElementById(c).style.cursor='default';document.onmousemove=function(){}},}}();
+document.addEventListener('DOMContentLoaded', function () {
+  var e = document.getElementById('panel');
+  e.setAttribute("onmousedown", "drag.startMoving(this, 'container', event);");
+  e.setAttribute("onmouseup", "drag.stopMoving('container');");
+  e.style.height = document.getElementById("editor").clientHeight - 17 + 'px';
+  e.style.width = document.getElementById("editor").clientWidth - 17 + 'px';
+});
+var drag = function () {
+  return {
+    move: function (d, x, y) {
+      d.style.left = x + 'px';
+      d.style.top = y + 'px';
+    },
+    startMoving: function (d, c, e) {
+      e = e || window.event;
+      var c = document.getElementById(c);
+      var x = e.clientX;
+      var y = e.clientY;
+      var l = d.style.left;
+      var t = d.style.top;
+      var eh = d.clientHeight;
+      var ew = d.clientWidth;
+      var ch = c.clientHeight;
+      var cw = c.clientWidth;
+      c.style.cursor = 'move';
+      l = l.replace('px', '');
+      t = t.replace('px', '');
+      var dx = x - l, dy = y - t;
+      document.onmousemove = function (e) {
+        e = e || window.event;
+        var x = e.clientX, y = e.clientY, ax = x - dx, ay = y - dy;
+        if (ax < 0) ax = 0;
+        if (ay < 0) ay = 0;
+        if (ax + ew > cw) ax = cw - ew;
+        if (ay + eh > ch) ay = ch - eh;
+        drag.move(d, ax, ay);
+        drag.move(document.getElementById("editor"), ax, ay);
+      };
+    },
+    stopMoving: function (c) {
+      var a = document.createElement('script');
+      document.getElementById(c).style.cursor = 'default';
+      document.onmousemove = function () { };
+    },
+  };
+} ();
--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-var d=document;function g(i){return d.getElementById(i);}function a(o,e,f){o.addEventListener(e,f);}function t(o){return o.getElementsByTagName('title')[0]}function f(){var e=g('editor');var r=g('result');a(e,'input',function(){r.srcdoc=e.value;var i=window.setInterval(function(){var c=t(r.contentDocument);if(c){clearInterval(i);t(document).innerHTML=c.innerHTML;};},100);});}a(d,'DOMContentLoaded',f);
+var d = document;
+function g(i) {
+  return d.getElementById(i);
+}
+function a(o, e, f) {
+  o.addEventListener(e, f);
+}
+function t(o) {
+  return o.getElementsByTagName('title')[0];
+}
+function f() {
+  var e = g('editor');
+  var r = g('result');
+  a(e, 'input', function () {
+    r.srcdoc = e.value;
+    var i = window.setInterval(function () {
+      var c = t(r.contentDocument);
+      if (c) {
+        clearInterval(i);
+        t(document).innerHTML = c.innerHTML;
+      };
+    }, 100);
+  });
+}
+a(d, 'DOMContentLoaded', f);
